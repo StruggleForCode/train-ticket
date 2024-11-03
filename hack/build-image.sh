3382ecf7b9b134e5cae9d001f8ed07f781a08169
StuggleForCode <leehao99@163.com>
2024-11-03 16:50:23 +0800

    Bug202411021329001 解决不了ts-avator-server DockerFile 只能暂时跳过

--- hack/build-image.sh
+++ hack/build-image.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 set -eux
 
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 <repo> <tag>"
+    exit 1
+fi
+
 echo
 echo "Start build images, Repo: $1, Tag: $2"
 echo
+
 for dir in ts-*; do
     if [[ -d $dir ]]; then
+        if [[ "$dir" == "ts-avatar-service" ]]; then
+            echo "Skipping directory: ${dir}"
+            continue  # 跳过 ts-avatar-service 目录
+        fi
+
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
-            echo "build ${dir}"
-            # docker build -t "$1"/"${dir}" "$dir"
-            # docker tag "$1"/"${dir}":latest "$1"/"${dir}":"$2"
-
-            # 直接使用指定标签构建镜像
+            echo "Building image for directory: ${dir}"
             docker build -t "$1"/"${dir}":"$2" "$dir"
-
+        else
+            echo "No Dockerfile found in directory: ${dir}"
         fi
     fi
 done
