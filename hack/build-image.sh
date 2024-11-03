0227ea61aac4fb185236bd434153f88198002f6c
StuggleForCode <leehao99@163.com>
2024-11-03 14:08:10 +0800

    Bug202411021329001 修改只构建一个镜像，不需要latest镜像

--- hack/build-image.sh
+++ hack/build-image.sh
@@ -8,9 +8,11 @@ for dir in ts-*; do
     if [[ -d $dir ]]; then
         if [[ -n $(ls "$dir" | grep -i Dockerfile) ]]; then
             echo "build ${dir}"
-            docker build -t "$1"/"${dir}" "$dir"
+            # docker build -t "$1"/"${dir}" "$dir"
             # docker tag "$1"/"${dir}":latest "$1"/"${dir}":"$2"
-            docker tag "$1"/"${dir}":"$2"
+
+            # 直接使用指定标签构建镜像
+            docker build -t "$1"/"${dir}":"$2" "$dir"
 
         fi
     fi
